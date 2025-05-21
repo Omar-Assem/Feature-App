0566d0508a61b5988e7aef5016ac59a99a8a1585
Omar-Assem <161471312+Omar-Assem@users.noreply.github.com>
2025-05-21 17:22:06 +0300

    k

--- src/Pages/Cat/Cat.jsx
+++ src/Pages/Cat/Cat.jsx
@@ -1,10 +1,61 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Cat = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("img/product.json");
+        setData(res.data);
+        console.log(data)
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
-      
-    </>
-  )
-}
+      {data.map((item, i) => (
+        <div className="col-lg-4" key={i}>
+          <div className="card">
+            <div className="offers text-center">
+              <span>${item.offers || ""}</span>
+            </div>
 
-export default Cat
+            <div className="img-continar">
+              <img src={item.overlay} alt={item.name} />
+              <img
+                src={`${item.overlay}.webp`}
+                className="overlay"
+                alt="overlay image"
+              />
+            </div>
+
+            <div className="icon">
+              <span>
+                <i className="fa-solid fa-heart"></i>
+              </span>
+              <span>
+                <i className="fa-solid fa-cart-shopping"></i>
+              </span>
+              <span>
+                <i className="fa-solid fa-circle-xmark"></i>
+              </span>
+            </div>
+          </div>
+
+          <div className="card-body">
+            <h5 className="card-title">{item.name}</h5>
+            <h5 className="card-price">{item.price}</h5>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Cat;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,66 +1,25 @@
-import CardHeader from "./CardHeader"
-import Future from "./Future"
-import Hero from "./Hero"
-import Testimonials from "./Testimonials"
-import Cards3 from "./Cards-3"
-import Carousel from "./Cursle"
-import Timer from "./Timer"
-import Gallery from "./Gallery"
-
-import axios from "axios";
-import { useEffect,useState } from "react";
+import CardHeader from "./CardHeader";
+import Future from "./Future";
+import Hero from "./Hero";
+import Testimonials from "./Testimonials";
+import Cards3 from "./Cards-3";
+import Carousel from "./Cursle";
+import Timer from "./Timer";
+import Gallery from "./Gallery";
+import Cat from "../Cat/Cat";
 
 const Home = () => {
-  const [data,setData] = useState([])
-  useEffect( () => {
- const fetchData = async()=>{
- try{
-  let res = await axios.get("img/product.json")
-  setData(res.data)
-  
- }catch(Err){
-  console.log(Err)
- }
- }
- fetchData()
-  }
-  , []);
-
   return (
     <>
-
-      <Hero/>
-      <CardHeader/>
-      <Future/>
-      <Cards3/>
-      <Testimonials/>
-      <Carousel/>
-      <Timer/>
-      <Gallery/>
-      <div className="">
-        <div className="row">
-          {data.slice(0,8).map((e,i)=>{
-            return(
-              <>
-                <div className="col-lg-3" key={i}>
-            <div className="card">
-              <div className="card-body" >
-              <img src={`${e.img}`} alt="" />
-              <h2>{e.name}</h2>
-              <p>{e.price}</p>
-              <p>{e.offers}</p>
-              <a className="text-dark" href="#">{e.category.join(" | ")}</a>
-              <p>{e.SKU}</p>
-              <p>{e.tag.join(" | ")}</p>
-              </div>
-            </div>
-          </div>
-              </>
-            )
-          })}
-        
-        </div>
-      </div>
+      <Hero />
+      <CardHeader />
+      <Future />
+      <Cards3 />
+      <Testimonials />
+      <Carousel />
+      <Timer />
+      <Gallery />
+      
     </>
   );
 };
