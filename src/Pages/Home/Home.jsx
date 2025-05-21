e80a4dea84f7067ce3572195fe4c6456c9cdcd51
Omar-Assem <161471312+Omar-Assem@users.noreply.github.com>
2025-05-21 16:48:07 +0300

    k

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,9 +7,28 @@ import Carousel from "./Cursle"
 import Timer from "./Timer"
 import Gallery from "./Gallery"
 
+import axios from "axios";
+import { useEffect,useState } from "react";
+
 const Home = () => {
+  const [data,setData] = useState([])
+  useEffect( () => {
+ const fetchData = async()=>{
+ try{
+  let res = await axios.get("img/product.json")
+  setData(res.data)
+  
+ }catch(Err){
+  console.log(Err)
+ }
+ }
+ fetchData()
+  }
+  , []);
+
   return (
     <>
+
       <Hero/>
       <CardHeader/>
       <Future/>
@@ -18,8 +37,32 @@ const Home = () => {
       <Carousel/>
       <Timer/>
       <Gallery/>
-      </>
-  )
-}
+      <div className="">
+        <div className="row">
+          {data.slice(0,8).map((e,i)=>{
+            return(
+              <>
+                <div className="col-lg-3" key={i}>
+            <div className="card">
+              <div className="card-body" >
+              <img src={`${e.img}`} alt="" />
+              <h2>{e.name}</h2>
+              <p>{e.price}</p>
+              <p>{e.offers}</p>
+              <a className="text-dark" href="#">{e.category.join(" | ")}</a>
+              <p>{e.SKU}</p>
+              <p>{e.tag.join(" | ")}</p>
+              </div>
+            </div>
+          </div>
+              </>
+            )
+          })}
+        
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
